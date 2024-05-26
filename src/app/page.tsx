f1964f4daa14e14302919f80fa69972ae2b2841a
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-05-26 09:19:13 +0700

    🆙update (page): usage toast Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Card from "@/components/Card";
+import Toast from "@/components/Toast";
 
 export default function Home() {
   return (
     <div>
+      <Toast />
       <Card />
     </div>
   );
