8f6f5a3baeb7735ecec70bc29492b24bf32c18aa
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-05-26 09:18:17 +0700

    💄update (card): add rounded

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import CardInfo from "./CardInfo";
 
 const Card = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="bg-white p-8 rounded-md ">
         <div className="flex flex-col gap-2 ">
-          <div className="bg-green-500 text-center p-1 w-44 text-md text-white font-semibold">Success</div>
-          <div className="bg-blue-500 p-1 text-md text-center text-white font-semibold">Info</div>
-          <div className="bg-orange-500  p-1 text-md text-center text-white font-semibold">Warning</div>
-          <div className="bg-red-500 p-1 text-md text-center text-white font-semibold">Error</div>
+          <button className="bg-green-500 text-center p-1 w-44 text-md text-white font-semibold rounded-sm">Success</button>
+          <button className="bg-blue-500 p-1 text-md text-center text-white font-semibold rounded-sm">Info</button>
+          <button className="bg-orange-500  p-1 text-md text-center text-white font-semibold rounded-sm">Warning</button>
+          <button className="bg-red-500 p-1 text-md text-center text-white font-semibold rounded-sm">Error</button>
         </div>
       </div>
     </div>
