79156dcf6f4a79aa9298034a742c32bd1cb33109
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-05-26 09:18:39 +0700

    ⭐new: create toast component

---
+++ src/components/Toast.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Toast = () => {
+  return (
+    <div className="bg-green-500 inline-block py-1 rounded-sm">
+      <div className="bg-white text-black inline-block px-20 py-2 rounded-sm font-semibold">
+        Success toast notification
+      </div>
+    </div>
+  );
+};
+
+export default Toast;
