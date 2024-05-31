e3ec259a13c88ee9932fe4dafc1cc69b00dea1e4
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-06-01 05:46:07 +0700

    💄update (toast) : remove bg

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Toast = () => {
   return (
-    <div className="bg-green-500 inline-block py-1 rounded-sm">
+    <div className="inline-block py-1 rounded-sm">
       <div className="bg-white text-black inline-block px-16 py-1 rounded-sm font-semibold">
         Success
       </div>
