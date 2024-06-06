c1a763b487f8e7e54d4d30bb7d9cf4a8e4bc587e
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-06-06 19:48:44 +0700

    ➖remove unused component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Card from "@/components/Card";
-import Toast from "@/components/Toast";
 
 export default function Home() {
   return (
     <div>
-      <Toast />
       <Card />
     </div>
   );
